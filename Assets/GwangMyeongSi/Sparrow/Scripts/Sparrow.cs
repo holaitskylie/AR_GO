7f17b11a8a5224584f9550cb3338053a4e137c4f
Seong Hyun Kang <ruanelroem@gmail.com>
2023-09-25 13:13:09 +0900

    Imported Required Assets for Project

---
+++ Assets/GwangMyeongSi/Sparrow/Scripts/Sparrow.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sparrow : MonoBehaviour
+{
+    private Animator sparrow;
+    public float gravity = 1.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sparrow = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterController.Move(moveDirection * Time.deltaTime);
+        moveDirection.y = gravity * Time.deltaTime;
+        if (sparrow.GetCurrentAnimatorStateInfo(0).IsName("idle1"))
+        {
+            sparrow.SetBool("preen", false);
+            sparrow.SetBool("peck", false);
+            sparrow.SetBool("fallin", false);
+            sparrow.SetBool("fly", false);
+            sparrow.SetBool("hop", false);
+        }
+        if (sparrow.GetCurrentAnimatorStateInfo(0).IsName("idle2"))
+        {
+            sparrow.SetBool("preen", false);
+            sparrow.SetBool("peck", false);
+        }
+        if (sparrow.GetCurrentAnimatorStateInfo(0).IsName("fly"))
+        {
+            sparrow.SetBool("takeoff", false);
+            sparrow.SetBool("landing", false);
+            sparrow.SetBool("hop", false);
+            sparrow.SetBool("hopleft", false);
+            sparrow.SetBool("hopright", false);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            sparrow.SetBool("preen", true);
+            sparrow.SetBool("idle1", false);
+            sparrow.SetBool("idle2", false);
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            sparrow.SetBool("peck", true);
+            sparrow.SetBool("idle1", false);
+            sparrow.SetBool("idle2", false);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            sparrow.SetBool("hop", true);
+            sparrow.SetBool("idle1", false);
+            sparrow.SetBool("idle2", false);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            sparrow.SetBool("idle1", true);
+            sparrow.SetBool("hop", false);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            sparrow.SetBool("hopleft", true);
+            sparrow.SetBool("hop", false);
+            sparrow.SetBool("idle1", false);
+            sparrow.SetBool("flyleft", true);
+            sparrow.SetBool("fly", false);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            sparrow.SetBool("hop", true);
+            sparrow.SetBool("hopleft", false);
+            sparrow.SetBool("idle1", true);
+            sparrow.SetBool("fly", true);
+            sparrow.SetBool("flyleft", false);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            sparrow.SetBool("hopright", true);
+            sparrow.SetBool("hop", false);
+            sparrow.SetBool("idle1", false);
+            sparrow.SetBool("flyright", true);
+            sparrow.SetBool("fly", false);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            sparrow.SetBool("hop", true);
+            sparrow.SetBool("hopright", false);
+            sparrow.SetBool("idle1", true);
+            sparrow.SetBool("fly", true);
+            sparrow.SetBool("flyright", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sparrow.SetBool("takeoff", true);
+            sparrow.SetBool("idle1", false);
+            sparrow.SetBool("idle2", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sparrow.SetBool("fly", false);
+            sparrow.SetBool("landing", true);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            sparrow.SetBool("fallin", true);
+            sparrow.SetBool("fly", false);
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            sparrow.SetBool("fly", true);
+            sparrow.SetBool("fallin", false);
+        }
+    }
+}
---
+++ Assets/GwangMyeongSi/Sparrow/Sparrow_readme.txt
@@ -0,0 +1,53 @@
+Sparrow 3D model.
+
+Include scene demo in URP
+13 Animations in root motion
+Added extra folder with In-place animations
+4 Textures (4096x4096)
+1 Shader -URP support-from Two Sided Shaders +URP package 
+(for technical support or buy complete package put in contact with the publisher )
+2 Materials -Support URP-
+1 Prefab
+1 Animator Controller
+1 Skybox
+3 Scripts
+1 Scene demo
+
+Low poly model
+
+2368 quad polygons on this mesh
+2723 vertices on this mesh
+
+Textures
+T_Sparrow_D
+T_Sparrow_N
+T_SparrowWings_D
+T_SparrowWings_N
+
+Animations List
+falling(390-425)
+fly(355-360)
+flyleft(428-433)
+flyright(436-441)
+hop(316-324)
+hopleft(444-452)
+hopright(455-463)
+idle1(0-200)
+idle2(200-245)
+landing(363-387)
+peck(278-313)
+preen(245-275)
+takeoff(327-352)
+
+Scene Demo
+keys for enable animations
+
+W=hop
+A=hopleft/flyleft
+D=hopright/flyright
+E=falling
+F=peck
+R=preen
+Space=takeoff/landing/fly
+
+enjoy game age:)
