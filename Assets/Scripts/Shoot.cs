f1089b509042ca700cd0576f5842ed325bf34cd4
holaitskylie <june2840@naver.com>
2023-10-27 20:05:39 +0900

    Mini Game
    1. 미니게임용 프리팹 생성(Prefab 폴더 - Mini Game)
    2. <Shoot.cs> 오디오소스 할당, catchclip 변수에 사운드 할당 / 사용할 사운드 에셋 임포트(Sound 폴더)
    3. <Shoot.cs> 에 SpawnManager의 Prefab에 할당한 프리팹만큼 Fire() 조건문 추가 / 미니게임용 프리팹에 태그 할당 완료
    4. Shoot 버튼, 크로스헤어 임포트
    5. 미니게임 프리팹에 박스 콜라이더 추가 / <Shoot>의 Fire() 메서드 적용되는지 확인해야함

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -3,58 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Shoot : MonoBehaviour
-{
-    public Camera camere;
+{  
+
+    public Camera camera;
     public GameObject prefab1;
 
-    //AudioSource audio;
+    AudioSource audio;
+    public AudioClip catchClip;
 
     void Start()
     {
-        //audio = GetComponent<AudioSource>();
-    }
-
-    public void Fire()
-    {
-        RaycastHit hit;
-        Ray ray = camere.ScreenPointToRay(Input.mousePosition);
-
-        if(Physics.Raycast(ray, out hit))
-        {
-            if(hit.transform.CompareTag("Animal"))
-            {
-                Destroy(hit.transform.gameObject);
-                Instantiate(prefab1, hit.point, Quaternion.LookRotation(hit.normal));
-                GameManager.instance.AddScore(50);
-            }
-
-            if(hit.transform.CompareTag("frog"))
-            {
-                Destroy(hit.transform.gameObject);
-                Instantiate(prefab1, hit.point, Quaternion.LookRotation(hit.normal));
-                GameManager.instance.AddScore(100);
-            }
-
-            if(hit.transform.CompareTag("dragonfly"))
-            {
-                Destroy(hit.transform.gameObject);
-                Instantiate(prefab1, hit.point, Quaternion.LookRotation(hit.normal));
-                GameManager.instance.AddScore(200);
-            }
-        }
+        audio = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
-        if(Input.touchCount > 0)
+        if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
 
-            if(touch.phase == TouchPhase.Began)
+            if (touch.phase == TouchPhase.Began)
             {
                 Fire();
             }
         }
     }
+
+    public void Fire()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))        
+        {        
+            if (hit.transform.CompareTag("Dragonfly"))            
+            {
+                Destroy(hit.transform.gameObject);
+                Instantiate(prefab1, hit.point, Quaternion.LookRotation(hit.normal));
+                GameManager.instance.AddScore(10);
+                audio.PlayOneShot(catchClip);
+            }
+
+            if (hit.transform.CompareTag("Frog"))
+            {
+                Destroy(hit.transform.gameObject);
+                Instantiate(prefab1, hit.point, Quaternion.LookRotation(hit.normal));
+                GameManager.instance.AddScore(50);
+                audio.PlayOneShot(catchClip);
+            }
+
+            if(hit.transform.CompareTag("Butterfly"))
+            {
+                Destroy(hit.transform.gameObject);
+                Instantiate(prefab1, hit.point, Quaternion.LookRotation(hit.normal));
+                GameManager.instance.AddScore(100);
+                audio.PlayOneShot(catchClip);
+            }
+
+            if (hit.transform.CompareTag("Honeybee"))
+            {
+                Destroy(hit.transform.gameObject);
+                Instantiate(prefab1, hit.point, Quaternion.LookRotation(hit.normal));
+                GameManager.instance.AddScore(150);
+                audio.PlayOneShot(catchClip);
+            }
+
+            if (hit.transform.CompareTag("Grasshopper"))
+            {
+                Destroy(hit.transform.gameObject);
+                Instantiate(prefab1, hit.point, Quaternion.LookRotation(hit.normal));
+                GameManager.instance.AddScore(200);
+                audio.PlayOneShot(catchClip);
+            }
+        }
+    }    
 }
